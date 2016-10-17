1c0d1e50ef1d7668a7929bc913c59f201d91e01a
Maru <marianotrebino@gmail.com>
2016-10-17 13:20:38 -0400

    Added HSV scheme color.

---
+++ Public/Utils/HSVColour.h
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   HSVColour.h
+ * Author: maru
+ *
+ * Created on October 17, 2016, 10:15 AM
+ */
+
+#ifndef HSVCOLOUR_H
+#define HSVCOLOUR_H
+
+#include <cstdint>
+class RGBColour;
+#include "Utils/RGBColour.h"
+
+class HSVColour {
+public:
+    HSVColour();
+    HSVColour(const RGBColour rgbColour);
+    HSVColour(const float hue, const float saturation, const float value);
+    
+    // Getters
+    const float hue() const { return m_hue; }
+    const float saturation() const { return m_saturation; }
+    const float value() const { return m_value; }
+    
+    // Operators
+    friend std::istream& operator >> (std::istream& is, HSVColour& colour);
+    friend std::ostream& operator << (std::ostream& os, HSVColour& colour);
+
+    
+    // Destructors
+    virtual ~HSVColour();
+private:
+    float m_hue,
+            m_saturation,
+            m_value;
+};
+
+#endif /* HSVCOLOUR_H */
+
