29ccad4c5f997469eb389cf3ea1aaf3d769829cb
Maru <marianotrebino@gmail.com>
2016-10-25 15:47:37 -0400

    Fixes direction and color function.

--- src/Point.cpp
+++ src/Point.cpp
@@ -21,11 +21,11 @@ Point::~Point(){
 }
 
 const Vector3D Point::get_direction(const Point3D& surface_point) const {
-    return (surface_point - m_position).hat();
+     return (m_position - surface_point).hat();
 }
 
 const RGBColor Point::get_color(const ShadeRec& sr) const {
-    const float distance_attenuator = 1/m_position.d_squared(sr.hit_point);
+    const float distance_attenuator = 100/m_position.distance(sr.hit_point);
     return m_intensity * m_color * distance_attenuator;
 }
         
